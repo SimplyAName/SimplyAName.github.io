a39ccb5ded807a4c1efdbf182cc17049b7780ebf
SimplyAName <michael.loney137@gmail.com>
2023-09-13 17:59:22 +0100

    init navbar and bg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 // TODO: Update this
 export default defineConfig({
   site: 'https://astronaut.github.io',
-})
+  integrations: [tailwind()]
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "simply-purple": "#4D2DB7",
+      },
+    },
+  },
+  plugins: [],
+};
